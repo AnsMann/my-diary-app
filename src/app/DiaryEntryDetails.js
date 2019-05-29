dd0d75dd6b06433620caf1da708f269277a8fc8a
Ansgar Reimann <ansgar.reimann@protonmail.com>
2019-05-29 15:14:38 +0200

    Minor changes after codereview

--- src/app/DiaryEntryDetails.js
+++ src/app/DiaryEntryDetails.js
@@ -13,7 +13,6 @@ const EntryDetails = styled.section`
   border-radius: 10px;
   margin: 30px 20px 20px;
   padding: 20px;
-  position: relativ;
   overflow: scroll;
   color: #007fbf;
   scroll h2,
