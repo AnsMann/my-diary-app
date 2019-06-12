d974010eba75f5d841aeb9dde3603ae16dbbf756
Ansgar Reimann <ansgar.reimann@protonmail.com>
2019-06-12 19:14:14 +0200

    Create mongoDB model

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const setupServer = require('./setup-server')
 const app = setupServer()
 
+const DiaryEntry = require('./models/DiaryEntry')
+
 // add your api here
-app.get('/test', (req, res) => {
-  res.json({ success: true })
+app.get('/diaryentries', (req, res) => {
+  DiaryEntry.find()
+    .then(entry => res.json(entry))
+    .catch(err => res.json(err))
 })
