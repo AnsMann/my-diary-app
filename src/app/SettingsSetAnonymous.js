0b21f1ad08fa3b442f3579fcfc74eb83de8dbc3c
Ansgar Reimann <ansgar.reimann@protonmail.com>
2019-06-12 19:57:24 +0200

    Add switch button

--- src/app/SettingsSetAnonymous.js
+++ src/app/SettingsSetAnonymous.js
@@ -1,6 +1,65 @@
 import React from 'react'
 import styled from 'styled-components'
 
+const SettingsBox = styled.section`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`
+
+const StyledLabel = styled.label`
+  color: #007fbf;
+  font-weight: bold;
+`
+
+const Switch = styled.label`
+  color: #007fbf;
+  position: relative;
+  display: inline-block;
+  width: 60px;
+  height: 34px;
+  input {
+    opacity: 0;
+    width: 0;
+    height: 0;
+  }
+`
+
+const Checkbox = styled.input.attrs(() => ({ type: 'checkbox' }))`
+  &:checked + span {
+    background-color: #2196f3;
+  }
+  &:focus + span {
+    box-shadow: 0 0 1px #2196f3;
+  }
+  &:checked + span:before {
+    transform: translateX(26px);
+  }
+`
+
+const StyledSpan = styled.span`
+  border-radius: 34px;
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: #ccc;
+  transition: 0.4s;
+  &:before {
+    border-radius: 50%;
+    position: absolute;
+    content: '';
+    height: 26px;
+    width: 26px;
+    left: 4px;
+    bottom: 4px;
+    background-color: white;
+    transition: 0.4s;
+  }
+`
+
+/*
 const Settingslabel = styled.label`
   color: #007fbf;
   font-weight: bold;
@@ -42,10 +101,26 @@ const Checkbox = styled.input.attrs(() => ({ type: 'checkbox' }))`
     content: '';
   }
 `
+*/
 
 export function SettingsSetAnonymous({ anonymousCheckboxStatus, onCheck }) {
   return (
-    <>
+    <SettingsBox>
+      <StyledLabel htmlFor="anymousCheckbox">Share as anonymous</StyledLabel>
+      <Switch>
+        <Checkbox
+          id="anymousCheckbox"
+          onChange={() => onCheck()}
+          checked={anonymousCheckboxStatus}
+        />
+        <StyledSpan />
+      </Switch>
+    </SettingsBox>
+  )
+}
+
+/* 
+<>
       <Checkbox
         id="anymousCheckbox"
         onChange={() => onCheck()}
@@ -55,5 +130,4 @@ export function SettingsSetAnonymous({ anonymousCheckboxStatus, onCheck }) {
         Share as anonymous
       </Settingslabel>
     </>
-  )
-}
+*/
